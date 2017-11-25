2083468d91fc2259c60385959f09fff6c6d0b742
ios_domain <1935722630@qq.com>
2017-11-25 15:05:22 +0800

    添加 '+' 按钮和它的点击事件

--- XZSwfitWB/Class/View/Main/XZMainViewController.swift
+++ XZSwfitWB/Class/View/Main/XZMainViewController.swift
@@ -13,23 +13,47 @@ class XZMainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //
+        // 创建子控制器
         setupChildControllers()
-        //
-        
+        // 创建 + 按钮
+        setupComposeButton()
     }
     
+    // MARK: - 按钮 '+' 的点击事件
+    // FIXME: 没有实现
+    @objc private func btnComposeClick() {
+        print("我是中间 '+' 按钮")
+    }
     
-    
+    // MARK: - 私有控件：'+' 按钮
+    private var btnCompose: UIButton = UIButton.xz_button(imgName: "tabbar_compose_icon_add",bgImg: "tabbar_compose_button")
 }
+
 
 // MARK: - 设置界面
 extension XZMainViewController {
-    
+    // 设置 '+' 按钮
+    private func setupComposeButton() {
+        tabBar.addSubview(btnCompose)
+        
+        // 计算按钮的宽度
+        let count = CGFloat(childViewControllers.count)
+        //
+        let width = tabBar.bounds.width / count - 1
+        
+        // CGRectInset 正数向内缩进，负数向外扩展 dy为负的话是像上去
+        btnCompose.frame = tabBar.bounds.insetBy(dx: width * 2, dy: 0)
+        
+        // 点击事件
+        btnCompose.addTarget(self, action: #selector(btnComposeClick), for: .touchUpInside)
+        
+    }
+
     // 设置所有的子控制器
     private func setupChildControllers() {
         let array = [["clsName":"XZHomeViewController","title":"首页","imgName":"home"],
                    ["clsName":"XZMessageViewController","title":"消息","imgName":"message_center"],
+                   ["clsName":"UIViewController"],
                    ["clsName":"XZDiscoverViewController","title":"发现","imgName":"discover"],
                    ["clsName":"XZProfileViewController","title":"我的","imgName":"profile"]
                   ]
