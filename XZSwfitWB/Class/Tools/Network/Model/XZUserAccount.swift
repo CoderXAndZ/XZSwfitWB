17bb97e89888c243010544750ab89720397632e0
ios_domain <1935722630@qq.com>
2017-12-15 17:34:48 +0800

    创建空的 UserModel

---
+++ XZSwfitWB/Class/Tools/Network/Model/XZUserAccount.swift
@@ -0,0 +1,13 @@
+//
+//  XZUserAccount.swift
+//  XZSwfitWB
+//
+//  Created by admin on 2017/12/15.
+//  Copyright © 2017年 XZ. All rights reserved.
+//
+
+import UIKit
+
+class XZUserAccount: NSObject {
+
+}
